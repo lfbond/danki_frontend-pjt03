24d0b708ede0fcc7904ad81b8b067bd6bf8600de
Luis Felipe de Souza Bond <69223872+lfbond@users.noreply.github.com>
2022-04-26 20:41:22 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,50 @@
+window.onload = function(){
+    
+    var map;
+
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-20.8202,-49.3797),
+            scrollwheel: false,
+            zoom:13,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        }
+
+        map = new google.maps.Map(document.getElementById("mapa"),mapProp);
+    }
+
+    function addMarker(lat,long,icon,content,click){
+        var latLng = {'lat':lat,'lng':long};
+
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map:map,
+            icon:icon
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content:content,
+            maxWidth:200,
+            pixelOffset: new google.maps.Size(0,20)
+        });
+
+        if(click == true){
+            google.maps.event.addListener(marker,'click',function(){
+                infoWindow.open(map,marker);
+            });
+        }else if(click == false){
+            infoWindow.open(map,marker);
+        }
+    }
+
+    initialize();
+
+    var conteudo = '<p style="color:black;font-size:13px;padding:10px 0;">Meu Endereço Personalizado!</p>';
+    addMarker(-20.828030,-49.378270,'',conteudo,true);
+
+
+    /* setTimeout(function(){
+        map.panTo({'lat':-23.550520,'lng':-46.633309});
+        map.setZoom(8); 
+    },4000); */
+}
